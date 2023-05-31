bb74c8d1a81fa73dec9bd683e204597c9252787b
DBass Design <info@dbassdesign.com>
2023-05-31 13:38:01 -0700

    Create Hello.py
    Former-commit-id: 3b3ef675eea59f8600dc4132e59d75be9329a1c6

---
+++ Hello.py
@@ -0,0 +1,4 @@
+# Autonomous-Design-Foundry-
+ ADFbots are proud to present one of our starting lineup stars... *+*+*CHEFFREY*+*+ Your personal and professional sous chef. 
+Cheffrey is a completely customizble autonomous agent brought to you by DB Design, LLC (2023). Try asking Cheffrey for one of your
+favorite recipes of try giving Cheffrey a list of ingredients you have on hand and ask for a dish recommendation. The possibilities are endless! 
