89facf6851cb884736f96403bfd91a3710641794
DBass Design <info@dbassdesign.com>
2024-06-08 17:01:53 -0400

    chore: catching up main to branch commit
    Former-commit-id: 3618864ba46f9f26b6cf3b2b3b7732a42491a03a

---
+++ adapt_backend/run 2.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.expanduser("~"))
+sys.path.append(os.path.join(os.path.expanduser("~"), "nomad"))
+
+from nomad import fetch_data
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/data', methods=['GET'])
+def fetch_data_endpoint():
+    raw_data = fetch_data()
+    processed_data = ... # Process the data using your Nomad functions
+    return jsonify({'result': processed_data})
+
+if __name__ == "__main__":
+    app.run(debug=True)
