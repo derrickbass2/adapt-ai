a69b71781a738ac0e6a725b467aaa86be51cbfb5
DBass Design <info@dbassdesign.com>
2024-08-15 18:40:14 -0400

    updating files for correct docker containerization

--- src/adapt_backend/models.py
+++ src/adapt_backend/models.py
@@ -1,17 +1,45 @@
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
+from sqlalchemy import Column
+import sklearn.ensemble
+
 from sklearn.svm import SVC
 
+
 # Define your ML models here
+class LogisticRegression:
+    pass
+
+    def __init__(self, max_iter=100):
+        self.model = sklearn.linear_model.LogisticRegression(max_iter=max_iter)
+
+
 class LRModel(LogisticRegression):
     def __init__(self):
-        super().__init__(solver='lbfgs', max_iter=1000)
+        super().__init__(max_iter=1000)
 
-class RFModel(RandomForestClassifier):
+
+class RFModel(sklearn.ensemble.RandomForestClassifier):
     def __init__(self):
-        super().__init__(n_estimators=100, random_state=42)
+        super().__init__(random_state=42)
+
 
 class SVMModel(SVC):
     def __init__(self):
-        super().__init__(kernel='rbf', C=1.0, random_state=42)
+        super().__init__(random_state=42)
 
+
+from base import Base
+
+
+class Integer:
+    pass
+
+
+class String:
+    pass
+
+
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
